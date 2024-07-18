b6aff5b0eb15ad70e46a53e04388e5f0a84c7a81
Amanjit Gill <amanjitgilldata@gmail.com>
2024-07-18 11:28:52 +1000

    fix cloud backup to delete unneeded files at dest

--- cloud_backup.sh
+++ cloud_backup.sh
@@ -8,9 +8,14 @@ source "$HOME/bin/backup_dirs.sh"
 date
 
 # specify each included source dir without '/'
-rsync -avzd \
+# -a: archive, ensures recursive 
+# -v: verbose 
+# -z: compress transmission
+# --delete: delete dest files that no longer exist at source 
+rsync -avz \
   --exclude software/ \
   --exclude venv*/ \
+  --delete \
   $HOME/Documents \
   $HOME/Downloads \
   $HOME/Pictures \
